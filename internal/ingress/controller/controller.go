caa79a73a10c4d7493a7e892bd1a1046dad0b9f9
Bryan Boreham <bryan@weave.works>
2018-12-21 17:10:28 +0000

    Add a flag to make per-host metrics optional
    When serving many hosts from one nginx, the metrics may become too
    numerous for Prometheus.  Add a flag to disable the host label, so
    that metrics are totalled across all hosts.
    
    Adds support for HTTP2 Push Preload annotation
    
    update test for backendprotocols
    
    Adds support for HTTP2 Push Preload annotation
    
    Signed-off-by: Ricardo Pchevuzinske Katz <ricardo.katz@serpro.gov.br>
    
    Adds support for HTTP2 Push Preload annotation
    
    Signed-off-by: Ricardo Pchevuzinske Katz <ricardo.katz@serpro.gov.br>
    
    Adds support for HTTP2 Push Preload annotation
    
    Signed-off-by: Ricardo Pchevuzinske Katz <ricardo.katz@serpro.gov.br>
    
    Adds support for HTTP2 Push Preload annotation
    
    Signed-off-by: Ricardo Pchevuzinske Katz <ricardo.katz@serpro.gov.br>
    
    Adds support for HTTP2 Push Preload annotation
    
    Adds support for HTTP2 Push Preload annotation
    
    Update nginx to 1.15.8 (#3601)
    
    Fix CLA URLs

--- internal/ingress/controller/controller.go
+++ internal/ingress/controller/controller.go
@@ -87,7 +87,8 @@ type Configuration struct {
 
 	EnableProfiling bool
 
-	EnableMetrics bool
+	EnableMetrics  bool
+	MetricsPerHost bool
 
 	EnableSSLChainCompletion bool
 
@@ -472,6 +473,7 @@ func (n *NGINXController) getBackendServers(ingresses []*ingress.Ingress) ([]*in
 						loc.ConfigurationSnippet = anns.ConfigurationSnippet
 						loc.CorsConfig = anns.CorsConfig
 						loc.ExternalAuth = anns.ExternalAuth
+						loc.HTTP2PushPreload = anns.HTTP2PushPreload
 						loc.Proxy = anns.Proxy
 						loc.RateLimit = anns.RateLimit
 						loc.Redirect = anns.Redirect
